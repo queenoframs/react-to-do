20612ccb9ab2eb3a48b0c835303824858bdfdb89
unknown <laura.m.stevens@gmail.com>
2018-06-07 19:12:08 -0500

    typo found to clear search box - assignment completed

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
        )}
       </ul>
       <form onSubmit={ (e) => this.handleSubmit(e) }>
-        <input type="text" value={ this.state.newTodoDesription } onChange={ (e) => this.handleChange(e) } />
+        <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
         <input type="submit" />
       </form>
       </div>
